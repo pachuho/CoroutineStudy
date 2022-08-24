fc48992ec9cebd4716c41541cc9632c1fffed272
박주호 <blueblack96@dynamicmedia.co.kr>
2022-08-24 13:20:23 +0900

    [Update] Dispatcher 종류

--- app/src/main/java/com/pachuho/coroutinestudy/MainActivity.kt
+++ app/src/main/java/com/pachuho/coroutinestudy/MainActivity.kt
@@ -8,9 +8,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        addJobLaunch()
+        addDispatcher()
     }
 
+    /**
+     * Dispatcher 종류
+     * Main: UI와 상호작용
+     * IO: 네트워크 작업
+     * Default: CPU를 많이 사용하는 작업
+     */
+    private fun addDispatcher(){
+        CoroutineScope(Dispatchers.Main).launch { // 기본 Dispatcher 설정
+            val gotDeferred: Deferred<Array<Int>> = async(Dispatchers.IO) {
+                arrayOf(5, 4, 3, 2, 1)
+            }
+
+            val sortedDeferred: Deferred<List<Int>> = async(Dispatchers.Default) {
+                val value = gotDeferred.await()
+                value.sortedBy { it }
+            }
+
+            launch {
+                val sortedArray = sortedDeferred.await()
+                println("textViewSettingJob, value: $sortedArray")
+            }
+        }
+    }
+
+    /**
+     * Dispatcher에 Coroutine 붙일때
+     * launch: 결과 값 반환 X, 변환 타입 = Job
+     * async: 결과 값 반환 O, 변환 타입 = Deferred<T>
+     */
     private fun addJobLaunch(){
         CoroutineScope(Dispatchers.Main).launch{
             val deferredInt = async {
